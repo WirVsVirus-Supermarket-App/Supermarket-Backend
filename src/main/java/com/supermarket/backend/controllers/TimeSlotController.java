f782cecfd0766078c141e0394fa6db41a33eddd7
Johannes Theiner <j.theiner@live.de>
2020-03-21 21:03:49 +0100

    added basic timeslots

--- src/main/java/com/supermarket/backend/controllers/TimeSlotController.java
+++ src/main/java/com/supermarket/backend/controllers/TimeSlotController.java
@@ -1,19 +1,54 @@
 package com.supermarket.backend.controllers;
 
+import com.supermarket.backend.entities.TimeSlot;
 import com.supermarket.backend.services.TimeSlotService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.time.LocalTime;
+import java.util.List;
 
 /**
- * @author Collin Alpert
+ * @author Johannes Theiner
  */
 @RestController
-@RequestMapping("/time-slot")
+@RequestMapping("/timeslot")
 public class TimeSlotController {
 
 	private final TimeSlotService timeSlotService;
 
 	public TimeSlotController(TimeSlotService timeSlotService) {
 		this.timeSlotService = timeSlotService;
+	}
+
+	@PostMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<Object> add(LocalTime begin, LocalTime end, long market) throws SQLException {
+		var slot = new TimeSlot(begin, end, market);
+		var id = this.timeSlotService.create(slot);
+		var fetchedSlot = this.timeSlotService.getById(id);
+		if(fetchedSlot.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping(value = "/request", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<Object> request(long timeslot) {
+		//TODO: wie wird das gespeichert
+		return ResponseEntity.ok().build();
+	}
+
+	@DeleteMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<Object> delete(long id) throws SQLException {
+		this.timeSlotService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+
+
+	@GetMapping(value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ResponseEntity<List<TimeSlot>> all(long market) {
+		return ResponseEntity.ok(this.timeSlotService.getMultiple(timeSlot -> timeSlot.getSupermarketId() == market).toList());
 	}
 }
